3496c85d30d553f52236b741df0abfa1ab46eda8
XabierBA <=>
2023-10-16 19:42:35 +0200

    Create fina exercise

---
+++ ejercicioFinal.py
@@ -0,0 +1,21 @@
+# ENUNCIADO
+
+# La representación de un cuaternión puede ser utilizada para describir una 
+# rotación en el espacio tridimensional. Es común convertir entre cuaterniones y 
+# ángulos de Euler (rotaciones alrededor de ejes individuales) en aplicaciones 
+# como la robótica y gráficos por computadora.
+
+# Esto se utiliza mucho al trabajar con orientaciones. 
+# Diseña una clase llamada Quaternions que facilite la transformación y manipulación de orientaciones:
+
+# MÉTODOS
+    # Constructor: Debe aceptar cuatro argumentos float (en el orden w, x, y, z) que representen los coeficientes del cuaternión.
+    # Getters y Setters
+    # Conversión de cuaternión a ángulos de Euler
+
+# CONVERSIÓN DE CUATERNIÓN A ÁGULOS DE EULER
+    # El método debe devolver:
+
+    # value0: Roll value.
+    # value1: Pitch value.
+    # value2: Yaw value.
