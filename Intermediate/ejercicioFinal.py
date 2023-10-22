d91c617becb3f6e22f9eca45574253a1f5627ef7
XabierBA <xabitic1@gmail.com>
2023-10-22 18:21:29 +0200

    Final commit

--- Intermediate/ejercicioFinal.py
+++ Intermediate/ejercicioFinal.py
@@ -5,73 +5,79 @@ la clase Quaternions del ejercicio final del módulo anterior del curso.
 
 Obligatoriamente debes usar al menos un método especial (sin incluir el constructor) 
 y anotaciones de tipo.
-
+      
 Por último debes llamar a esta clase en un módulo distinto."""
 
 import math as mh
 
 class Quaternions:
 
-    def __init__(self, w, x, y, z):
-            self.__w = w
-            self.__x = x
-            self.__y = y
-            self.__z = z
+    def __init__(self, w: float, x: float, y: float, z: float):
+        # Normalize the quaternion
+        norm = mh.sqrt(w**2 + x**2 + y**2 + z**2)
+        if norm == 0:
+            raise ValueError("Quaternion norm is zero")
+        self.__w = w / norm
+        self.__x = x / norm
+        self.__y = y / norm
+        self.__z = z / norm
 
-    #GETTERS AND SETTERS
+    # GETTERS AND SETTERS
     @property
     def w(self) -> float:
-          return self.__w  
+        return self.__w
+
     @w.setter
-    def w(self, w:float) -> None:
-          self.__w = w
+    def w(self, w: float) -> None:
+        self.__w = w
 
     @property
     def x(self) -> float:
-          return self.__x
+        return self.__x
+
     @x.setter
-    def x(self, x:float) -> None:
-          self.__x = x
+    def x(self, x: float) -> None:
+        self.__x = x
 
     @property
     def y(self) -> float:
-          return self.__y 
+        return self.__y
+
     @y.setter
-    def y(self, y:float) -> None:
-          self.__y = y
+    def y(self, y: float) -> None:
+        self.__y = y
 
     @property
     def z(self) -> float:
         return self.__z
-    @z.setter
-    def z(self, z:float) -> None:
-          self.__z = z
 
-    
-      
-    def pitch(self):
+    @z.setter
+    def z(self, z: float) -> None:
+        self.__z = z
+
+    def pitch(self) -> float:
         # Calculate pitch (x-axis rotation)
         sinP = 2 * (self.w * self.y - self.x * self.z)
         return mh.asin(sinP)
-
-    def yaw(self):
+    def yaw(self) -> float:
         # Calculate yaw (z-axis rotation)
         sinY = 2.0 * (self.w * self.z + self.x * self.y)
         cosY = 1.0 - (2.0 * (self.z * self.z + self.y * self.z))
         return mh.atan2(sinY, cosY)
 
-    def roll(self):
+    def roll(self) -> float:
         # Calculate roll (y-axis rotation)
         sinR = 2.0 * (self.w * self.x + self.y * self.z)
         cosR = 1.0 - (2.0 * (self.x * self.x + self.y * self.y))
         return mh.atan2(sinR, cosR)
 
+    def __str__(self) -> str:
+        return f"Quaternion(w={self.w}, x={self.x}, y={self.y}, z={self.z})"
 
-
-
-quat = Quaternions(1,2,3,4) 
+quat = Quaternions(1, 2, 3, 4)
 
 print("EULER ANGLES IN RADIANS:")
-print(f" Value0(Roll): {quat.roll()}\n",
+print(f"{quat}\n")
+print(f"Value0(Roll): {quat.roll()}\n",
       f"Value1(Pitch): {quat.pitch()}\n",
       f"Value2(Yaw): {quat.yaw()}\n")
--- Mathematics with Python/ejercicioFinal.py
+++ Mathematics with Python/ejercicioFinal.py
@@ -23,3 +23,82 @@ Deberá ser nombrada como Trazada intermedia.
 El resultado final del ejercicio debe ser la representación en 2 dimensiones de las tres líneas, 
 con sus respectivos nombres mostrados en pantalla y un título que describa adecuado el proceso realizado.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# Function that reads the file where the coordinates of the Nurburgring circuit are
+def read_coords(nombre_archivo):
+    x_values = []
+    y_values = []
+    with open(nombre_archivo, 'r') as archivo_csv:
+        lector_csv = csv.DictReader(archivo_csv)
+        for fila in lector_csv:
+            x = float(fila['x_m'])
+            y = float(fila['y_m'])
+            x_values.append(x)
+            y_values.append(y)
+    return np.array(x_values), np.array(y_values)
+
+# Function that calculates the left limit of the track
+def left_offset(x, y, distancia):
+    #Calculate displacement vectors to create parallel traces
+    dx = np.gradient(x)
+    dy = np.gradient(y)
+    norm = np.sqrt(dx**2 + dy**2)
+    dx = dx / norm
+    dy = dy / norm
+
+    # Calculate parallel coordinates
+    x_left = x + distancia * dy
+    y_left = y - distancia * dx
+
+    return x_left, y_left
+
+# Function that calculates the right limit of the track
+def right_track(x, y, distancia):
+    # Calculate displacement vectors to create parallel traces
+    dx = np.gradient(x)
+    dy = np.gradient(y)
+    norm = np.sqrt(dx**2 + dy**2)
+    dx = dx / norm
+    dy = dy / norm
+
+    # Calculate parallel coordinates
+    x_right = x + distancia * dy
+    y_right = y - distancia * dx
+
+    return x_right, y_right
+
+# Name of the CSV file containing the coordinates
+archivo_coord = "nurburgring.csv"
+
+# Read 'x' and 'y' coordinates from CSV file and save them to separate NumPy arrays
+x_mid, y_mid = read_coords(archivo_coord)
+
+# Distance to trace parallel lines(Variables can be changed)
+offset1 = 10.0
+offset2 = -10.0
+
+# Get left limit of the track (Blue cones)
+x_left, y_left = left_offset(x_mid, y_mid, offset1)
+
+
+# Get right limit of the track (Yellow cones)
+x_right, y_right = right_track(x_mid, y_mid, offset2)
+
+
+# Representation of the track limits
+plt.plot(x_right, y_right, c="yellow", linestyle=":", label="Right Limit (Yellow cones)")
+plt.plot(x_left, y_left, c="blue", linestyle=":", label="Left Limit (Blue cones)")
+
+# Representation of the path of the autonomous car
+plt.plot(x_mid, y_mid, c="g", label="Path")
+
+plt.xlabel('Coord x')
+plt.ylabel('Coord y')
+plt.title('Nurburgring circuit path planing')
+plt.legend()
+plt.grid(False)
+plt.show()
